82553bfaddead9d95e3e1ecdd35b0303af0b64c3
晚风 <2486195095@qq.com>
2024-04-01 17:04:20 +0800

    Initial commit
    Signed-off-by: 晚风 <2486195095@qq.com>

---
+++ src/Book/test1.java
@@ -0,0 +1,16 @@
+/*
+ * 太原科技大学。
+ * Copyright 2021, YCHS Information&Science Technology Co.,Ltd,
+ * All rights reserved.
+ */
+package Book;
+
+/**
+ * @author liuchao
+ * @version 1.0
+ * @date 2024/4/1
+ */
+
+
+public class test1 {
+}
